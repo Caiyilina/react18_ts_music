e822a0f0b93b2ce47ec0d6b282451d5cdff552bc
cyl <2893176150@qq.com>
2024-12-02 17:54:19 +0800

    less配置

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,15 @@
-const path = require("path");
-const resolve = (dir) => path.resolve(__dirname, dir);
+const path = require("path")
+const CracoLessPlugin = require("craco-less")
+const resolve = dir => path.resolve(__dirname, dir)
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin
+    }
+  ],
   webpack: {
     alias: {
-      "@": resolve("src"),
-    },
-  },
-};
+      "@": resolve("src")
+    }
+  }
+}
